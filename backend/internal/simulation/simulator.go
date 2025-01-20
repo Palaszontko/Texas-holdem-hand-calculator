8b4ee7e297feb0a256cf839065574f51738638f2
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-01-20 13:50:05 +0100

    feat(simulator): implement simulation logic to calculate win/loss probabilities

---
+++ backend/internal/simulation/simulator.go
@@ -0,0 +1,103 @@
+package simulator
+
+import (
+	"sync"
+
+	"github.com/Palaszontko/texas-holdem-hand-calculator/backend/internal/poker"
+)
+
+type Simulator struct {
+	config Config
+}
+
+func NewSimulator(config Config) *Simulator {
+	return &Simulator{config: config}
+}
+
+func (s *Simulator) RunSimulation() (*Result, error) {
+	results := make(chan poker.Result, s.config.NumIterations)
+
+	var wg sync.WaitGroup
+
+	iterationsPerWorker := s.config.NumIterations / s.config.NumConcurrent
+
+	for i := 0; i < s.config.NumConcurrent; i++ {
+		wg.Add(1)
+		go s.simulationWorker(iterationsPerWorker, results, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	wins, losses, ties := 0, 0, 0
+
+	for result := range results {
+		switch result {
+		case poker.Win:
+			wins++
+		case poker.Lose:
+			losses++
+		case poker.Tie:
+			ties++
+		}
+	}
+
+	totalIterations := float64(wins + losses + ties)
+
+	return &Result{
+		WinProbability:  float64(wins) / totalIterations,
+		LoseProbability: float64(losses) / totalIterations,
+		TieProbability:  float64(ties) / totalIterations,
+		Iterations:      int(totalIterations),
+	}, nil
+
+}
+
+func (s *Simulator) simulationWorker(iterations int, results chan<- poker.Result, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 0; i < iterations; i += 1 {
+		result := s.runSingleSimulation()
+		results <- result
+	}
+}
+
+func (s *Simulator) runSingleSimulation() poker.Result {
+	deck := poker.NewDeck()
+	deck = s.removeKnownCards(deck)
+	deck.Shuffle()
+
+	opponentHand := poker.NewHand(deck.Draw(2)...)
+	remainingCommunityCards := 5 - len(s.config.CommunityCards)
+
+	communityCards := append(s.config.CommunityCards, deck.Draw(remainingCommunityCards)...)
+
+	result, _, _ := poker.CompareHands(s.config.PlayerHand, opponentHand, communityCards)
+
+	return result
+}
+
+func (s *Simulator) removeKnownCards(deck poker.Deck) poker.Deck {
+	knownCards := make(map[poker.Card]bool)
+
+	for _, card := range s.config.PlayerHand.Cards {
+		knownCards[card] = true
+	}
+
+	for _, card := range s.config.CommunityCards {
+		knownCards[card] = true
+	}
+
+	var remainingCards []poker.Card
+
+	for _, card := range deck.Cards {
+		if !knownCards[card] {
+			remainingCards = append(remainingCards, card)
+		}
+	}
+
+	return poker.Deck{Cards: remainingCards}
+
+}
