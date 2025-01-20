a1a4b27da65cde03e526398eed1a0e1bcd37e37c
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-01-20 15:11:09 +0100

    feat(server): implement HTTP server with health check and simulation handlers

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/Palaszontko/texas-holdem-hand-calculator/backend/internal/api/handlers"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/api/health", handlers.HealthCheckHandler)
+	http.HandleFunc("/api/simulation", handlers.SimulationHander)
+
+	fmt.Println("Starting server on port 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
 }
