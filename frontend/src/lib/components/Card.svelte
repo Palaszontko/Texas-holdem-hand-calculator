57ffe44e1bb87d71216a88b0f9514ca8b76fdf7d
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-01-24 02:32:53 +0100

    fix(frontend): adjust height of Card component for better display

--- frontend/src/lib/components/Card.svelte
+++ frontend/src/lib/components/Card.svelte
@@ -2,6 +2,6 @@
     export let src;
 </script>
 
-<div class="inline-block w-24 h-36 hover:scale-110 transition cursor-pointer">
+<div class="w-24 h-32 inline-block hover:scale-110 transition cursor-pointer">
     <img {src} alt="Card" class="w-full h-full object-contain"/>
 </div>
