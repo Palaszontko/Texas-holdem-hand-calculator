895d1bad097635aa912a029ae754fdde99bc6ee2
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-01-24 01:50:08 +0100

    feat(frontend): add Card component with dynamic image source

---
+++ frontend/src/lib/components/Card.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let src;
+</script>
+
+<div class="inline-block w-24 h-36 hover:scale-110 transition cursor-pointer">
+    <img {src} alt="Card" class="w-full h-full object-contain"/>
+</div>
