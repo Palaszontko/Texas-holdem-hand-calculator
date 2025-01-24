d5fe03a42bd9a45d71253539a654220457677d6b
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-01-24 02:32:32 +0100

    feat(frontend): add EmptyCardSlot component for placeholder display

---
+++ frontend/src/lib/components/EmptyCardSlot.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Card from "./Card.svelte";
+
+</script>
+
+<div class="inline-block w-36 h-48 m-0.5 bg-[#808080] rounded border-2 border-gray-400">
+    <div class="w-full h-full">
+    </div>
+</div>
