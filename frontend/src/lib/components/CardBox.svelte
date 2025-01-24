a58573515f6849d1df247c333b0b7587116d7b16
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-01-24 01:50:11 +0100

    feat(frontend): add CardBox component for flexible content display

---
+++ frontend/src/lib/components/CardBox.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div class="px-2 py-1 my-2 mx-4 shadow-lg rounded-lg bg-[#c8c8c8]">
+    <div class="flex flex-wrap gap-2 justify-start">
+        <slot></slot>
+    </div>
+</div>
