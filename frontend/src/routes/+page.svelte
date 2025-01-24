6507894b657aaef25c4fa8e5b1e8d8a21283552d
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-01-24 01:50:05 +0100

    feat(frontend): implement card display with dynamic SVG loading

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import CardBox from "$lib/components/CardBox.svelte";
+    import Card from '$lib/components/Card.svelte';
+
+    const cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    const suits = ['S', 'H', 'D', 'C'];
+
+    const getCards = (suit) => {
+        const result = cards.map((card) => `/cards/${card}${suit}.svg`);
+        return result;
+    };
+
+    const diamondCards = getCards('D');
+    const clubCards = getCards('C');
+    const heartCards = getCards('H');
+    const spadeCards = getCards('S');
+
+    diamondCards.pop();
+
+    const allCards = [diamondCards, clubCards, heartCards, spadeCards];
+    
+</script>
+
+
+
+<div class="min-h-screen bg-[#3f3f3f] flex flex-col items-center p-4">
+    <div class="cards-container max-w-[1400px]">
+        {#each allCards as cardSetColor}
+        <CardBox>
+            {#each cardSetColor as card}
+            <Card src={card}></Card>
+            {/each}
+        </CardBox>
+        {/each}
+    </div>
+</div>
